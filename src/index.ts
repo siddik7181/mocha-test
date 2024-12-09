89be0d4c48ade67a369644d5de70899c5b206a59
Nahid <Abu.Siddik@optimizely.com>
2024-12-10 01:14:57 +0600

    crud operations

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import connectDB from './db.config';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRoute from './routes/User';
 
+dotenv.config();
 const app = express();
 
 const port: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript + Node.js + Express!');
+app.use(cors());
+app.use(express.json());
+
+app.use('/users', userRoute);
+
+// if no endpoints found
+app.use((req: Request, res: Response, next: NextFunction)=> {
+  next({
+    status: 404,
+    message: "No Routes Found"
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.use((err: ErrorType, req: Request, res: Response, next: NextFunction) => {
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Issue";
+  console.error(err);
+  res.status(status).json({status, message});
+  return;
 });
+
+type ErrorType = {
+  status: number,
+  message: string
+}
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+};
+
+startServer();
